30b0f490ea0dea7e792ca4510c1beb60ab70ccd0
AnkitAdhikari <ankitadhikari31512@gmail.com>
2025-10-14 11:51:36 +0545

    updated addon image validation

--- backend/models/addon.js
+++ backend/models/addon.js
@@ -1,45 +1,47 @@
-const {Model, DataTypes, Sequelize} = require("sequelize");
+const { Model, DataTypes, Sequelize } = require("sequelize");
 
 module.exports = (sequelize) => {
-    class Addon extends Model{
-        static associate(models){
-            // Many-to-many relationship with Product
-            Addon.belongsToMany(models.productModel, {
-                through: 'products_addons',
-                foreignKey: 'addonId',
-                otherKey: 'productId',
-                as: 'products'
-            });
-        }
+  class Addon extends Model {
+    static associate(models) {
+      // Many-to-many relationship with Product
+      Addon.belongsToMany(models.productModel, {
+        through: "products_addons",
+        foreignKey: "addonId",
+        otherKey: "productId",
+        as: "products",
+      });
     }
+  }
 
-    Addon.init({
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        price: {
-            type: DataTypes.DECIMAL,
-            allowNull: false,
-        },
-        imageUrl: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        
-    }, {
-        sequelize,
-        modelName: "addon",
-        tableName: "addons",
-        timestamps: true,
-        createdAt: "createdAt",
-        updatedAt: "updatedAt",
-    })
+  Addon.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "addon",
+      tableName: "addons",
+      timestamps: true,
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  );
 
-    return Addon
-}
+  return Addon;
+};
